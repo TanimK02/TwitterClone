0243b825d13a06e5bb7adde60d97f1630b68eca4
Tanim Khan <khantanim5640@gmail.com>
2024-09-17 22:56:21 -0700

    sidenav added

---
+++ twitter-clone/src/app/page.tsx
@@ -0,0 +1,10 @@
+import SideNav from "./ui/SideNav";
+
+export default function Home() {
+  return (
+    <>
+    <SideNav></SideNav>
+
+    </>
+  );
+}
---
+++ twitter-clone/src/app/ui/SideNav.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import NavLinks from './NavLinks';
+import '@/app/ui/SideNav.css'
+import Image from 'next/image';
+import plus from '@/public/plus.svg'
+
+export default function SideNav() {
+    return (
+        <div className='sideNav-div'>
+            <div className='nav-list'>
+                <Link href='/' className='logo'>
+                <h1>X</h1>
+                </Link>
+                <NavLinks></NavLinks>
+                <button><p>Post</p><Image src={plus} alt="post tweet" height={30} width={30}></Image></button>
+            </div>
+        </div>
+    )
+}
